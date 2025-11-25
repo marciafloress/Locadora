5f24a2842830a62084182604d0a5a99bce64809d
Donalson-pdjour <holojk9@gmail.com>
2025-11-25 20:45:16 -0300

    class moto atualizada

--- Moto.java
+++ Moto.java
@@ -1,5 +1,45 @@
+
 public class Moto extends Veiculo {
-    public double calcularTotal(int dias) {
-        return preco + dias;
+
+    protected String pezinho;
+    protected String capacete;
+
+    public Moto(String modelo, double precoDiaria, int ano, int kmRodados, String chassi, String placa, String cor,
+            String pezinho, String capacete) {
+        super(modelo, precoDiaria, ano, kmRodados, chassi, placa, cor);
+        this.pezinho = pezinho;
+        this.capacete = capacete;
+
     }
+
+    public void setPezinho(String pezinho) {
+        this.pezinho = pezinho;
+    }
+
+    public String getPezinho() {
+        return pezinho;
+    }
+
+    public void setCapacete(String capacete) {
+        this.capacete = capacete;
+    }
+
+    public String getCapacete() {
+        return capacete;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto [Pezinho:  " + getPezinho()
+                + ", Capacete:  " + getCapacete()
+                + ", Modelo:  " + getModelo()
+                + ", Preço Diaria:  " + getPrecoDiaria()
+                + ", Ano:  " + getAno()
+                + ", Km Rodados:  " + getKmRodados()
+                + ", Chassi:  " + getChassi()
+                + ", Placa:  " + getPlaca()
+                + ", Cor:  " + getCor() + "]";
+
+    }
+
 }
